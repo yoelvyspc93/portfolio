18a12b61d2b2c3773b309e72c97b067e1000a647
Yoelvys Perez Cabrera <yoelvys.perez@dspot.pl.com>
2025-01-25 13:57:25 -0500

    refactor: comment out unused section imports in HomeLayout component

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react';
 
-import { About } from '@/components/sections/home/About';
-import { Contact } from '@/components/sections/home/Contact';
-import { Experience } from '@/components/sections/home/Experience';
+// import { About } from '@/components/sections/home/About';
+// import { Contact } from '@/components/sections/home/Contact';
+// import { Experience } from '@/components/sections/home/Experience';
 import { Header } from '@/components/sections/home/Header';
-import { Projects } from '@/components/sections/home/Projects';
-import { Skills } from '@/components/sections/home/Skills';
+// import { Projects } from '@/components/sections/home/Projects';
+// import { Skills } from '@/components/sections/home/Skills';
 
 export default function HomeLayout({
   modal,
@@ -15,11 +15,11 @@ export default function HomeLayout({
   return (
     <main id="main">
       <Header />
-      <About />
+      {/* <About />
       <Projects />
       <Experience />
       <Skills />
-      <Contact />
+      <Contact /> */}
       {modal}
     </main>
   );
