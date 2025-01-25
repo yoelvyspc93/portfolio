e964f324c6d849f5ee8c140bd217094c2a349a88
Yoelvys Perez Cabrera <yoelvys.perez@dspot.pl.com>
2025-01-25 14:07:07 -0500

    refactor: comment out unused imports in layout and navigator components

--- src/constants/social.tsx
+++ src/constants/social.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
-import GithubIcon from '@/../public/icons/github.svg';
-import LinkedinIcon from '@/../public/icons/linkedin.svg';
+// import GithubIcon from '@/../public/icons/github.svg';
+// import LinkedinIcon from '@/../public/icons/linkedin.svg';
 
 export interface SocialLink {
   icon: ReactNode;
@@ -11,12 +11,14 @@ export interface SocialLink {
 
 export const socialLinks: SocialLink[] = [
   {
-    icon: <GithubIcon />,
+    // icon: <GithubIcon />,
+    icon: <div>GitHub</div>,
     platform: 'GitHub',
     url: 'https://github.com/yoelvyspc93',
   },
   {
-    icon: <LinkedinIcon />,
+    // icon: <LinkedinIcon />,
+    icon: <div>LinkedIn</div>,
     platform: 'LinkedIn',
     url: 'https://www.linkedin.com/in/yoelvys-perez-cabrera',
   },
