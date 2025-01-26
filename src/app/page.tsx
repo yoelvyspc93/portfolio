910a5abae3a46c8bb77e076ea636ede76d15f1e6
Yoelvys Perez Cabrera <yoelvys.perez@dspot.pl.com>
2025-01-25 22:23:53 -0500

    refactor: replace SvgArrow with Image component in HomePage and handle production path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import SvgArrow from '@/public/icons/arrow.svg';
+//import arrow from '@/public/icons/arrow.svg';
+import Image from 'next/image';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default function HomePage() {
   return (
     <div>
       <h1>
         Yoelvys
-        {/* <Image src={img} alt="avatar" width={200} height={200} unoptimized /> */}
-        <SvgArrow width={200} height={200} />
+        <Image
+          src={`${isProduction ? '/portfolio' : ''}/icons/arrow.svg`}
+          alt="Vercel Logo"
+          width={100}
+          height={24}
+          priority
+        />
       </h1>
     </div>
   );
