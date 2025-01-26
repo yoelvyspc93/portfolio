2f7599e6fcf520479c4d57dc9720c419cd148047
Yoelvys Perez Cabrera <yoelvys.perez@dspot.pl.com>
2025-01-25 22:55:07 -0500

    refactor: remove unused SVG icons and update icon imports in HomePage

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Yoelvys</h1>
+    </div>
+  );
+}
---
+++ src/components/common/Icons/CypressIcon.tsx
@@ -0,0 +1,21 @@
+import { SVGProps } from 'react';
+
+const CypressIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg fill="none" viewBox="0 0 42 42" {...props}>
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3.5}
+      d="M34.09 29.762A15.75 15.75 0 1 0 21 36.75a3.64 3.64 0 0 0 3.454-2.49l6.171-18.51m-7 0 3.5 10.5"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3.5}
+      d="M18.837 16.47a5.25 5.25 0 1 0-.04 9.082"
+    />
+  </svg>
+);
+export default CypressIcon;
